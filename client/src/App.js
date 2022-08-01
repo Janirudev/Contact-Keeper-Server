353d30baaa5f81259e457882d2b1eb4b71214e9f
Janiru Vishmith <me@janiru.dev>
2022-08-01 13:04:40 +0400

    Alert Context, State and Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home.js';
 import About from './components/pages/About.js';
 import Register from './components/auth/Register.js';
@@ -8,6 +9,7 @@ import Login from './components/auth/Login.js';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
@@ -15,19 +17,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/about" component={About}></Route>
-                <Route exact path="/register" component={Register}></Route>
-                <Route exact path="/login" component={Login}></Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/about" component={About}></Route>
+                  <Route exact path="/register" component={Register}></Route>
+                  <Route exact path="/login" component={Login}></Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
